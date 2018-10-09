0f03b1308f5c5a4e52ccb28ce45acd77d9661740
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-09 04:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, { [key]: value })
 }
-function deleteFromObjectByKey(object, key){
-  var clone = object
-  delete clone[key]
-  return clone
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone.key;
+  return clone;
 }
