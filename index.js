4355d0dba793f77f537e13992f7ae26352b3eeda
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-09 04:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var recipes = {}
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
-}
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({},object,{key:value})
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
