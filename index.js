80314dd21821e1d5d3828a7f3665722c9f8d2ce9
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-09 04:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function deleteFromObjectByKey(object, key) {
   delete clone[key]
   return clone
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
